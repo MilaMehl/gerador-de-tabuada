d600672ed023d4c3f6dc28ae56ec5ef5fd1249fc
Camila Lima <milafmhel@gmail.com>
2025-09-08 12:01:33 -0300

    Ultimas modificações do código, implementei a função que calcula a tabuada até n com um intervalo de 0.5s a cada conta feita

--- gerador_tabuada.py
+++ gerador_tabuada.py
@@ -1,10 +1,23 @@
 import time
+#importa a biblioteca de tempo
 
-def main():
-    print(f'---Gerador de tabuada---')
+print(f'--- Gerador de tabuada ---')
+#vai anunciar no terminal que é um gerador de tabuada
 
-while True:
-    numero = int(input('Digite o número da tabuada:'))
-    n = int(input('Digite quantas multiplicações você deseja: '))
+numero = int(input('Digite o número da tabuada: '))
+#pergunta qual a tabuada que o usuário quer
 
-    print(f'------------------------ \n Resultado é: {conta} \n------------------------')
+n = int(input('Digite até quanto calcular: '))
+# pergunta até qual número calcular a tabuada
+
+def conta (numero, n):
+    for i in range(1, n + 1):
+        print(f'{i} x {numero} = {numero * i}')
+        time.sleep(0.5)
+#cria um laço de repetição pra calcular a tabuada, logo ele mostra o resultado do calculo e dá um intervalo de 0.5s (da biblioteca tempo)
+
+conta(numero, n)
+#chama a função pra ela aparecer no terminal
+
+print('---- fim da tabuada ----')
+#anuncia o fim da tabuada
